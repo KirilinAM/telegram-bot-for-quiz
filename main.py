3454014c096b948dba516fbe42de57f23856ecbc
Artem Kirilin <kirilinartem@yandex.ru>
2025-01-03 04:01:51 +0700

    Выбранный выриант остается

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from aiogram.filters.command import Command
 from aiogram import types
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
+import json
 
 from database import create_table, update_quiz_index, get_quiz_index
 
@@ -54,7 +55,6 @@ async def new_quiz(message):
 
 
 async def get_question(message, user_id):
-
     # Запрашиваем из базы текущий индекс для вопроса
     current_question_index = await get_quiz_index(user_id)
     # Получаем индекс правильного ответа для текущего вопроса
@@ -74,14 +74,17 @@ def generate_options_keyboard(answer_options, right_answer):
     builder = InlineKeyboardBuilder()
 
     # В цикле создаем 4 Inline кнопки, а точнее Callback-кнопки
-    for option in answer_options:
+    for i, option in enumerate(answer_options):
+        callback_data = json.dumps({
+            'is_right': option == right_answer, 'num': i, 'id': 1
+        })
         builder.add(types.InlineKeyboardButton(
             # Текст на кнопках соответствует вариантам ответов
             text=option,
             # Присваиваем данные для колбэк запроса.
             # Если ответ верный сформируется колбэк-запрос с данными 'right_answer'
             # Если ответ неверный сформируется колбэк-запрос с данными 'wrong_answer'
-            callback_data="right_answer" if option == right_answer else "wrong_answer")
+            callback_data=callback_data)
         )
 
     # Выводим по одной кнопке в столбик
@@ -89,8 +92,14 @@ def generate_options_keyboard(answer_options, right_answer):
     return builder.as_markup()
 
 
-@dp.callback_query(F.data == "right_answer")
-async def right_answer(callback: types.CallbackQuery):
+@dp.callback_query(F.data.contains('"id": 1'))
+async def answer_callback(callback: types.CallbackQuery):
+    # Получаем данные колбека
+    callback_data = json.loads(callback.data)
+
+    # Получаем текст кнопки
+    text = callback.message.reply_markup.inline_keyboard[callback_data['num']][0].text
+
     # редактируем текущее сообщение с целью убрать кнопки (reply_markup=None)
     await callback.bot.edit_message_reply_markup(
         chat_id=callback.from_user.id,
@@ -100,39 +109,18 @@ async def right_answer(callback: types.CallbackQuery):
 
     # Получение текущего вопроса для данного пользователя
     current_question_index = await get_quiz_index(callback.from_user.id)
-
-    # Отправляем в чат сообщение, что ответ верный
-    await callback.message.answer("Верно!")
-
-    # Обновление номера текущего вопроса в базе данных
-    current_question_index += 1
-    await update_quiz_index(callback.from_user.id, current_question_index)
-
-    # Проверяем достигнут ли конец квиза
-    if current_question_index < len(quiz_data):
-        # Следующий вопрос
-        await get_question(callback.message, callback.from_user.id)
-    else:
-        # Уведомление об окончании квиза
-        await callback.message.answer("Это был последний вопрос. Квиз завершен!")
-
-
-@dp.callback_query(F.data == "wrong_answer")
-async def wrong_answer(callback: types.CallbackQuery):
-    # редактируем текущее сообщение с целью убрать кнопки (reply_markup=None)
-    await callback.bot.edit_message_reply_markup(
-        chat_id=callback.from_user.id,
-        message_id=callback.message.message_id,
-        reply_markup=None
-    )
-
-    # Получение текущего вопроса для данного пользователя
-    current_question_index = await get_quiz_index(callback.from_user.id)
-
     correct_option = quiz_data[current_question_index]['correct_option']
+    correct_answer = quiz_data[current_question_index]['options'][correct_option]
 
-    # Отправляем в чат сообщение об ошибке с указанием верного ответа
-    await callback.message.answer(f"Неправильно. Правильный ответ: {quiz_data[current_question_index]['options'][correct_option]}")
+    # Дублируем в чате ответ
+    await callback.message.answer(f"Выбранный вариант: {text}")
+
+    # Отправляем в чат оценку ответа
+    if callback_data['is_right']:
+        await callback.message.answer("Верно!")
+    else:
+        await callback.message.answer(
+            f"Неправильно. Правильный ответ: {correct_answer}")
 
     # Обновление номера текущего вопроса в базе данных
     current_question_index += 1
